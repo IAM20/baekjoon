b327f7c21ec2421efed5dcbdc71d3d5b76aa3d17
ydLee <game9777@naver.com>
2018-08-09 00:02:14 +0900

    2579

--- 2579_bj/main.cpp
+++ 2579_bj/main.cpp
@@ -4,16 +4,34 @@
  * Author : yd-lee
  * DP
  */
+ typedef struct {
+     int value;
+     int B4;
+     int B4B4;
+ } value;
 
- int dp[301];
+ value dp[301];
+
+ void func(int n) {
+     dp[1].B4 = dp[1].B4B4 = dp[1].value;
+     dp[2].B4 = dp[1].value + dp[2].value;
+     dp[2].B4B4 = dp[2].value;
+
+     for(int i = 3; i <= n; i++) {
+         dp[i].B4 = dp[i - 1].B4B4 + dp[i].value;
+         dp[i].B4B4 = ((dp[i - 2].B4 > dp[i - 2].B4B4) ? dp[i - 2].B4 : dp[i - 2].B4B4) + dp[i].value;
+     }
+ }
 
  int
  main(int argc, char * argv[]) {
      int n;
      scanf("%d", &n);
      for (int i = 1; i <= n; i++) {
-         scanf("%d", &dp[i]);
+         scanf("%d", &dp[i].value);
      }
+     func(n);
+     printf("%d\n", (dp[n].B4 > dp[n].B4B4) ? dp[n].B4 : dp[n].B4B4);
 
-     for ()
- }
+     return 0;
+}
