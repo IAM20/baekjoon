5f20d9c4e937aa89cac2f631ff2bd95bb3784a98
201300264Lee <2013008264@hanyang.ac.kr>
2018-09-03 16:20:25 +0900

    1011 update

--- 1011_bj/main.c
+++ 1011_bj/main.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
+#include <math.h>
 
-int
-func(int distance) {
-    int speed = 2;
-    int retval = 2;
-    switch(distance) {
-        case 1:
-            return 1;
-        case 2:
-            return 2;
-        case 3:
-            return 3;
-        default:
-            
-    }
+long long getK(long long dist) {
+	long long retval;
+	double tmp = ((-1 + (sqrt(1 + 4 * dist))) / 2);
+	retval = (long long)tmp;
+	if(tmp - retval == 0)
+		return retval;
+	else
+		return ++retval;
+}
 
-    return retval;
+long long
+func(long long dist) {
+	long long k;
+	switch(dist) {
+		case 1:
+			return 1;
+		case 2:
+			return 2;
+		default:
+			k = getK(dist);
+			return (dist - (k - 1) * k > k) ? (2 * k) : (2 * k) - 1;
+	}
 }
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+	int iter;
+	long long x, y;
+	scanf("%d", &iter);
+	for(int i = 0; i < iter; i++) {
+		scanf("%lld %lld", &x, &y);
+		printf("%lld\n", func(y - x));
+	}
 }
